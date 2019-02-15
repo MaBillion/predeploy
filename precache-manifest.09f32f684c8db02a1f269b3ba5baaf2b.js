c7090df32816648dd0ffd1f0f2af7f569261b66a
chao.ma <chao.ma@brilliantaero.com>
2019-02-15 20:04:31 +0800

    Updates

---
+++ precache-manifest.09f32f684c8db02a1f269b3ba5baaf2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "bfe067b34865c409a8e2",
+    "url": "/static/js/main.bfe067b3.chunk.js"
+  },
+  {
+    "revision": "49f556b8a14dd067f0ab",
+    "url": "/static/js/2.49f556b8.chunk.js"
+  },
+  {
+    "revision": "bfe067b34865c409a8e2",
+    "url": "/static/css/main.495833b1.chunk.css"
+  },
+  {
+    "revision": "49f556b8a14dd067f0ab",
+    "url": "/static/css/2.c5188896.chunk.css"
+  },
+  {
+    "revision": "59b0012fd7dbce3d47c54b8c02fe116b",
+    "url": "/index.html"
+  }
+];
